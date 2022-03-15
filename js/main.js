e689e7bb0ef48771ac77d546b92529845b3cb54d
Sherzodxon <yarmatxonovshezodxon@gmail.com>
2022-03-15 20:34:39 +1400

    remoting dark lite

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-const btn = document.querySelector(".navbar__white-btn");
-const white = document.querySelector("body");
-const up = document.querySelector(".navbar__in-btn");
-const inn = document.querySelector(".navbar__up-btn");
-const a = document.querySelector("a");
-const navlink = document.querySelector(".nav__item-link");
-const navlink1 = document.querySelector(".nav__item-link-one");
-const navlink2 = document.querySelector(".nav__item-link-two");
-const navlink3 = document.querySelector(".nav__item-link-three");
-const wbtn = document.querySelector(".header__hamburger--opened");
-const wxxbtn = document.querySelector(".header__hamburger--closed");
+// const btn = document.querySelector(".navbar__white-btn");
+// const white = document.querySelector("body");
+// const up = document.querySelector(".navbar__in-btn");
+// const inn = document.querySelector(".navbar__up-btn");
+// const a = document.querySelector("a");
+// const navlink = document.querySelector(".nav__item-link");
+// const navlink1 = document.querySelector(".nav__item-link-one");
+// const navlink2 = document.querySelector(".nav__item-link-two");
+// const navlink3 = document.querySelector(".nav__item-link-three");
+// const wbtn = document.querySelector(".header__hamburger--opened");
+// const wxxbtn = document.querySelector(".header__hamburger--closed");
 
-btn.addEventListener("click", function () {
-    btn.classList.toggle("navbar__white-btn-black");
-    white.classList.toggle("black-body");
-    up.classList.toggle("navbar__in-btn--black");
-    inn.classList.toggle("navbar__up-btn--black");
-    a.classList.toggle("a-white"); 
-    navlink.classList.toggle("nav__item-link--white");
-    navlink1.classList.toggle("nav__item-link--white-one");
-    navlink2.classList.toggle("nav__item-link--white-two");
-    navlink3.classList.toggle("nav__item-link--white-three");
-    wbtn.classList.toggle("header__hamburger-white--opened");
-    wxxbtn.classList.toggle("header__hamburger-white--closed");
-})
+// btn.addEventListener("click", function () {
+//     btn.classList.toggle("navbar__white-btn-black");
+//     white.classList.toggle("black-body");
+//     up.classList.toggle("navbar__in-btn--black");
+//     inn.classList.toggle("navbar__up-btn--black");
+//     a.classList.toggle("a-white"); 
+//     navlink.classList.toggle("nav__item-link--white");
+//     navlink1.classList.toggle("nav__item-link--white-one");
+//     navlink2.classList.toggle("nav__item-link--white-two");
+//     navlink3.classList.toggle("nav__item-link--white-three");
+//     wbtn.classList.toggle("header__hamburger-white--opened");
+//     wxxbtn.classList.toggle("header__hamburger-white--closed");
+// })
+function theme() {
+    const toggleTheme = document.querySelector(".header__hamburger--opened");
+    toggleTheme = addEventListener("click", () => {
+        document.documentElement.setAttribute('data-theme','dark');
+    })
+}
+theme()
